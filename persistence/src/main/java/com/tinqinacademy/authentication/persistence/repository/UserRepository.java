cd0cd929912ebe641fb5701b3844bee37b766817
Bogomil Stoev <bogomil.stoev01@gmail.com>
2024-08-07 13:38:20 +0300

    Create UserRepository.java

---
+++ persistence/src/main/java/com/tinqinacademy/authentication/persistence/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.tinqinacademy.authentication.persistence.repository;
+
+import com.tinqinacademy.authentication.persistence.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
