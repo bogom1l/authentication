a478a5d53eeeedad7ee44ef4f8450d90a7f53c97
Bogomil Stoev <bogomil.stoev01@gmail.com>
2024-08-21 12:47:04 +0300

    Added models

---
+++ api/src/main/java/com/tinqinacademy/authentication/api/operations/recoverpassword/RecoverPasswordOutput.java
@@ -0,0 +1,12 @@
+package com.tinqinacademy.authentication.api.operations.recoverpassword;
+
+import com.tinqinacademy.authentication.api.base.OperationOutput;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Builder
+@ToString
+public class RecoverPasswordOutput implements OperationOutput {
+}
