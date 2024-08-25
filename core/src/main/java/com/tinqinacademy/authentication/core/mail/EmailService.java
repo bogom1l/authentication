69684d1bcbf5d4a7a6daa80db1b8183c148fd7be
bogom1l <bogomil.stoev01@gmail.com>
2024-08-25 17:03:00 +0300

    Implemented JavaMailSender

--- core/src/main/java/com/tinqinacademy/authentication/core/mail/EmailService.java
+++ core/src/main/java/com/tinqinacademy/authentication/core/mail/EmailService.java
@@ -1,19 +1,24 @@
 package com.tinqinacademy.authentication.core.mail;
 
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class EmailService {
-    private final JavaMailSender emailSender;
 
-    public void sendSimpleMessage(String to, String subject, String text) {
+    @Autowired
+    private JavaMailSender emailSender;
+
+    @Value("${spring.mail.username}")
+    private String FROM;
+
+    public void sendEmail(String to, String subject, String text) {
         SimpleMailMessage message = new SimpleMailMessage();
 
-        message.setFrom("noreply@baeldung.com"); // todo
+        message.setFrom(FROM);
         message.setTo(to);
         message.setSubject(subject);
         message.setText(text);
